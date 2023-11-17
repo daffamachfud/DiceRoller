14099e7bc2dac5a1445a58040a0babc5461f8ba5
Daffa Radifanka <daffa.radifanka@dretail.id>
2023-11-17 16:47:30 +0700

    implement good practice

--- app/src/main/java/com/daffa/jetpack/diceroller/MainActivity.kt
+++ app/src/main/java/com/daffa/jetpack/diceroller/MainActivity.kt
@@ -19,16 +19,28 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun rollDice() {
-        val diceImage : ImageView = findViewById(R.id.imageDice)
+        // Create new Dice object with 6 sides and roll the dice
         val dice = Dice(6)
-        when (dice.roll()) {
-            1 -> diceImage.setImageResource(R.drawable.dice_1)
-            2 -> diceImage.setImageResource(R.drawable.dice_2)
-            3 -> diceImage.setImageResource(R.drawable.dice_3)
-            4 -> diceImage.setImageResource(R.drawable.dice_4)
-            5 -> diceImage.setImageResource(R.drawable.dice_5)
-            else -> diceImage.setImageResource(R.drawable.dice_6)
+        val diceRoll = dice.roll()
+
+        // Find the ImageView in the layout
+        val diceImage: ImageView = findViewById(R.id.imageDice)
+
+        // Determine which drawable resource ID to use based on the dice roll
+        val drawableResource = when (diceRoll) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
         }
+
+        // Update the ImageView with the correct drawable resource ID
+        diceImage.setImageResource(drawableResource)
+
+        // Update the content description
+        diceImage.contentDescription = diceRoll.toString()
     }
 }
 
